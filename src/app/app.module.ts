c7bb3024859e0ec6666f63585afe7ae4d60e8355
J-Guenther <jannik.guentheg@gmail.com>
2023-07-16 19:22:38 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RecipeTableComponent } from './recipe-table/recipe-table.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, RecipeTableComponent],
+    imports: [BrowserModule, BrowserAnimationsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recipe-table/recipe-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-recipe-table',
+    templateUrl: './recipe-table.component.html',
+    styleUrls: ['./recipe-table.component.css']
+})
+export class RecipeTableComponent {}
